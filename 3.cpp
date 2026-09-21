ae9f8fc8f74bae15684e0709c50dbc41a28f3754
BLONDI <aikudinova2008@gmail.com>
2026-09-21 23:40:38 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  
+using namespace std;
+int main() {
+    int A;            // переменная A, принимает значение 0 или 1
+    cin >> A;         // считываем A
+    int B = 2 - A; 
+    cout << B;       
+    return 0;         
+}
