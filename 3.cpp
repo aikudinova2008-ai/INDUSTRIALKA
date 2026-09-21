1f16d7ad3b46ec7688834ed37730bab5ae65d9e0
BLONDI <aikudinova2008@gmail.com>
2026-09-21 23:39:22 +0300

    Delete 3.cpp

--- 3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>  
-using namespace std;
-int main() {
-    int A;            // переменная A, принимает значение 0 или 1
-    cin >> A;         // считываем A
-    int B = 2 - A; 
-    cout << B;       
-    return 0;         
-}
